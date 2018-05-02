759beb3ccdab97c415e0d911273305cae7dd286a
s20a3264 <s20a3264@hotmail.com>
2018-05-03 00:39:27 +0800

    0.0.4

--- lib/reals.rb
+++ lib/reals.rb
@@ -6,16 +6,34 @@ module Reals
 
   class Application
     def call(env)
-      #暫時擋一下
-      if env['PATH_INFO'] == '/favicon.ico'
-        return [404, {'Content-Type' => 'text/html'}, []]
-      end
+      begin
 
-      klass, act = get_controller_and_action(env)
-      controller = klass.new(env)
-      text = controller.send(act)
-      [200, {'Content-Type' => 'text/html'},
-        [text]]
+        #暫時擋一下
+        if env['PATH_INFO'] == '/favicon.ico'
+          return [404, {'Content-Type' => 'text/html'}, []]
+        end
+
+        #root temp
+        if env['PATH_INFO'] == '/'
+          env['PATH_INFO'] = '/home/index'
+        end
+
+        #302
+        if env['PATH_INFO'] == '/302_test'
+          return [302, {'Location' => '/home/index'}, []]
+        end
+
+        klass, act = get_controller_and_action(env)
+        controller = klass.new(env)
+        text = controller.send(act)
+        [200, {'Content-Type' => 'text/html'},
+          [text]]
+
+      rescue
+        html = File.read("app/views/500.html")
+        [500, {'Content-Type' => 'text/html'},
+          [html]]
+      end
     end
   end
 
